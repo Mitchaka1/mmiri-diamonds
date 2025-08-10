fb239d10125974e95ea019a8658a97235d6876d7
Mitchaka1 <akaelumitchell@gmail.com>
2025-08-10 01:52:13 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './App.css';
+
+// Create the root element and render the React app.  The BrowserRouter
+// component enables client-side routing throughout the application.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
